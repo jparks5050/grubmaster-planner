179cdf8e8218674c16977177d72c95d8a0eff1ca
jparks5050 <jparks5050@gmail.com>
2025-10-07 16:04:38 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+// src/main.jsx
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App'
-import './index.css'
-import ErrorBoundary from './ErrorBoundary.jsx'
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// If you use Firebase, ensure initialization happens in App.jsx or a separate firebase.js file.
+// This entry simply mounts the root React component.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
